a00348ce92545bc88b0feb3a383cf5e39facd619
jailcomfranssa <jail_son_0@hotmail.com>
2022-12-23 19:37:47 -0300

    first commit criando projeto

---
+++ src/main/java/jailcomfranssa/github/com/SpringBootTreinamentoApplication.java
@@ -0,0 +1,13 @@
+package jailcomfranssa.github.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTreinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTreinamentoApplication.class, args);
+	}
+
+}

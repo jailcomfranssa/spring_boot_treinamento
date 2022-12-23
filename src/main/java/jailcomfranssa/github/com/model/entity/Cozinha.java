fb492256bd7f57cb14220d374585bf86072b1e7c
jailcomfranssa <jail_son_0@hotmail.com>
2022-12-23 20:54:38 -0300

    criando entidades e relacionamento ManyToOne

---
+++ src/main/java/jailcomfranssa/github/com/model/entity/Cozinha.java
@@ -0,0 +1,36 @@
+package jailcomfranssa.github.com.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/jailcomfranssa/github/com/model/entity/Restaurante.java
@@ -0,0 +1,70 @@
+package jailcomfranssa.github.com.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

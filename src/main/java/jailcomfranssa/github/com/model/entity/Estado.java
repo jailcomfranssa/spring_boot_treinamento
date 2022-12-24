a749c323f10cd7214580e1884900c75974444365
jailcomfranssa <jail_son_0@hotmail.com>
2022-12-24 03:45:07 -0300

    criando entidade estado

---
+++ src/main/java/jailcomfranssa/github/com/model/entity/Estado.java
@@ -0,0 +1,21 @@
+package jailcomfranssa.github.com.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+}

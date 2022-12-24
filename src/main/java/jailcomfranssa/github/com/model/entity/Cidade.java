9ec060f5589b7742d37b46af37ddbbad8b795a91
jailcomfranssa <jail_son_0@hotmail.com>
2022-12-24 03:41:39 -0300

    criando entidades

---
+++ src/main/java/jailcomfranssa/github/com/model/entity/Cidade.java
@@ -0,0 +1,21 @@
+package jailcomfranssa.github.com.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @ManyToOne
+    Estado estado;
+}
